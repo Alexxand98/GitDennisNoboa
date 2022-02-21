b58379517885db7b198fc7d032bb39e015db628d
denny <denny@DESKTOP-MEQO0ET>
2022-02-21 18:54:56 +0100

    Primer getJugador

---
+++ src/DennisNoboaBasket.java
@@ -0,0 +1,14 @@
+
+public class DennisNoboaBasket {
+
+	public String jugador;
+	public int njugador;
+	public String nombreHobby;
+	
+	public DennisNoboaBasket(String jugador, int njugador, String nombreHobby) {
+		super();
+		this.jugador = jugador;
+		this.njugador = njugador;
+		this.nombreHobby = nombreHobby;
+	}
+}
